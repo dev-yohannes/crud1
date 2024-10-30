8a1ff63cd63d5aae2cf01ee98b576b73e8ce1d98
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2024-10-30 20:49:38 +0300

    creating the structure of the create client page containing inputs and buttons

---
+++ create.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Crud1</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" name="name" value="" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" name="email" value="" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" name="phone" value="" class="form-control">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" name="address" value="" class="form-control">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a href="index.php" class="btn btn-outline-primary" role="button">Cancel</a>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</body>
+
+</html>

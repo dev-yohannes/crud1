92f8d9d015babf64806d47a7c4a1206006dd15f5
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2024-10-30 17:10:07 +0300

    added the base ui (table) for the users list with edit and delete action buttons, and added a static data to view until the data comes from the database

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Crud1</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>List of clients</h2>
+        <a class="btn btn-primary" href="create.php" role="button">New Client</a> <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Created At</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>10</td>
+                    <td>Bill Gates</td>
+                    <td>bill.gates@microsoft.com</td>
+                    <td>+22323342423</td>
+                    <td>New York, USA</td>
+                    <td>18/05/22</td>
+                    <td>
+                        <a class="btn btn-primary btn-sm" href="edit.php">Edit</a>
+                        <a class="btn btn-danger btn-sm" href="delete.php">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>

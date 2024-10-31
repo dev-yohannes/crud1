7da453d7777e50f5476ad61ac7d0d7f206a42c47
dev-yohannes <yohannesmulu.gm.dev@gmail.com>
2024-10-31 11:40:08 +0300

    displaying the errorMessage and successMessage if both variables are not empty, and added bootstrap js script to close those error messages

--- create.php
+++ create.php
@@ -39,11 +39,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>Crud1</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
     <div class="container my-5">
         <h2>New Client</h2>
+
+        <?php
+        if (!empty($errorMessage)) {
+            echo "
+            <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+            </div>
+            ";
+        }
+        ?>
+
         <form method="post">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Name</label>
@@ -69,6 +82,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <input type="text" name="address" value="<?php echo $address ?>" class="form-control">
                 </div>
             </div>
+
+            <?php
+            if (!empty($successMessage)) {
+                echo "
+                <div class='row mb-3'>
+                    <div class='offset-sm-3 col-sm-6'>
+                        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                            <strong>$successMessage</strong>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Success'></button>
+                        </div>
+                    </div>
+                </div>
+                ";
+            }
+            ?>
 
             <div class="row mb-3">
                 <div class="offset-sm-3 col-sm-3 d-grid">
